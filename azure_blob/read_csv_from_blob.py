63b2c81639d5d9fb4ca0760892e72b4aa168bd67
Nisha <46855901+Nisha789@users.noreply.github.com>
2025-02-13 17:06:04 +0530

    Add read_csv_from_blob file

---
+++ azure_blob/read_csv_from_blob.py
@@ -0,0 +1,30 @@
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+from io import StringIO
+
+# Azure Blob Storage account credentials
+account_name = 'stgaccsample'
+account_key = 'abcd'
+container_name = 'salesdata'
+blob_name = 'customer_subscription.csv'
+
+# Connection string (you can use this instead of account name and key)
+connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+
+# Initialize BlobServiceClient
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+# Get the container client
+container_client = blob_service_client.get_container_client(container_name)
+
+# Get the blob client
+blob_client = container_client.get_blob_client(blob_name)
+
+# Download blob content as a string
+blob_data = blob_client.download_blob().content_as_text()
+
+# Read the content into a pandas Dataframe
+df = pd.read_csv(StringIO(blob_data))
+
+# Display the dataframe
+print(df)
